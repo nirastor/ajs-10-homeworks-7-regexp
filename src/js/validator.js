692cc0ef150677a7469615fb3c897fe8bcc5fa38
Nikita Rastorguev <nirastor@gmail.com>
2020-09-29 20:50:31 +0300

    validator grammatic update

--- src/js/validator.js
+++ src/js/validator.js
@@ -1,8 +1,8 @@
 export default class Validator {
   static validateUsername(name) {
     const hasNotAllowedChars = /[^a-zA-Z\d_-]/.test(name);
-    const hasMoreThreeDigintsTogether = /\d{4,}/.test(name);
-    const startAndEndWithWrongChars = /^[\d_-]|[\d_-]$/.test(name);
-    return !hasNotAllowedChars && !hasMoreThreeDigintsTogether && !startAndEndWithWrongChars;
+    const hasMoreThreeDigitsTogether = /\d{4,}/.test(name);
+    const startOrEndWithWrongChars = /^[\d_-]|[\d_-]$/.test(name);
+    return !hasNotAllowedChars && !hasMoreThreeDigitsTogether && !startOrEndWithWrongChars;
   }
 }
