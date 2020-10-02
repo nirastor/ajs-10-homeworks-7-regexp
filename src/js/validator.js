48e664997d4d31ab7bdf814b0f6d232c69371645
Nikita Rastorguev <nirastor@gmail.com>
2020-10-02 12:34:10 +0300

    lecturers update

--- src/js/validator.js
+++ src/js/validator.js
@@ -1,8 +1,22 @@
 export default class Validator {
   static validateUsername(name) {
+    // better with \w and "i" flag
+    // better use has
     const hasNotAllowedChars = /[^a-zA-Z\d_-]/.test(name);
     const hasMoreThreeDigitsTogether = /\d{4,}/.test(name);
     const startOrEndWithWrongChars = /^[\d_-]|[\d_-]$/.test(name);
     return !hasNotAllowedChars && !hasMoreThreeDigitsTogether && !startOrEndWithWrongChars;
   }
 }
+
+/*
+// use \w for diapasones
+// more humanic: invert 1st and 3rd conditions
+
+static validateUsername(name) {
+  const hasAllowedChars = /^[\w-]$/.test(name);
+  const hasMoreThreeDigitsTogether = /\d{4,}/.test(name);
+  const startOrEndAllowedChars = /^[a-z]|[a-z]$/i.test(name);
+  return hasAllowedChars && !hasMoreThreeDigitsTogether && startAndEndAllowedChars;
+}
+*/
